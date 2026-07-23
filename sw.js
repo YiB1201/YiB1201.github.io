84d67aad00f785b2962c4fb8921d965c531048c8
YiB1201 <2313964396@qq.com>
2026-07-23 18:12:46 +0800

    Site updated: 2026-07-23 18:12:46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1781940589883";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1784801345532";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
